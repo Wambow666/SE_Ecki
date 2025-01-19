cfa10f69cbba6ac62c0972ac2ca602ff8e0b1d7a
Daniel Kitschke <daniel.kitschke00@gmail.com>
2025-01-19 22:35:31 +0100

    finally

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,14 +22,20 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    for (int prime : primeNumbers) {
+      assertEquals("Test prime number " + prime, true, PrimeCheck.isPrime(prime));
+    }
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] nonPrimes = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20};
+    for (int nonPrime : nonPrimes) {
+      assertEquals("Test non-prime number " + nonPrime, false, PrimeCheck.isPrime(nonPrime));
+    }
   }
 }
+
+// Diese Aufgabe wurd in Zusammenarbeit mit Sasha Ulrich und Daniel Kitschke erarbeitet
